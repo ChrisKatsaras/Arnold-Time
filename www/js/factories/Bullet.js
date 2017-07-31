56db0b9025ed86300c56e154fa7de666ff19a1ee
Christopher Katsaras <ckatsara@mail.uoguelph.ca>
2017-07-31 02:22:12 -0400

    Bullet seems to come out of gun pretty much perfectly now

--- www/js/factories/Bullet.js
+++ www/js/factories/Bullet.js
@@ -15,9 +15,10 @@ angular.module('Game.factories')
 	BulletFactory.prototype = { 
 		draw: function () {
 			var x = this.x - 6;//Half the bullet width
+			var y = this.y - 10;//Half the bullet height
 			var div = angular.element('<div id="'+this.bulletID+'"class="bullet"></div>');
 			this.body = angular.element(document.querySelector('#field'))
-			div.css('transform','translate3d('+x+'px,'+this.y+'px,0px) rotate('+this.angle+'rad)');
+			div.css('transform','translate3d('+x+'px,'+y+'px,0px) rotate('+this.angle+'rad)');
 			this.body.append(div);
 
 			//this.person = angular.element(document.querySelector('#'+this.bu));
