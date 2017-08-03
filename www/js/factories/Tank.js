20d0b8a469fd62f54b4c664d469cbe8e172c4f64
Christopher Katsaras <ckatsara@mail.uoguelph.ca>
2017-08-02 20:16:00 -0400

    Think I have implemented a very basic and functional health bar now

--- www/js/factories/Tank.js
+++ www/js/factories/Tank.js
@@ -29,7 +29,7 @@ angular.module('Game.factories')
 
 		draw : function(){
 			var div = angular.element('<div id="'+this.id+'"class="tank tank1"><div id="holder-'+this.id+'" class="point"></div></div>');
-			var healthbar = angular.element('<div id="health-bar'+this.id+'"class="health-bar"><div class="health-bar-glass"><div class="health-bar-fluid"></div></div></div>');
+			var healthbar = angular.element('<div id="health-bar'+this.id+'"class="health-bar"><div id="health-bar-glass'+this.id+'" class="health-bar-glass"><div id="health-bar-fluid'+this.id+'" class="health-bar-fluid"></div></div></div>');
 			//this.body = angular.element(document).find('body');
 			this.body = angular.element(document.querySelector('#field'))
 			this.body.append(div);
@@ -50,6 +50,8 @@ angular.module('Game.factories')
 		refresh : function () {
 			angular.element(document.querySelector('#'+this.id)).css('transform','translate3d('+this.x+'px,'+this.y+'px,0px) rotate('+this.angle+'rad)');
 			angular.element(document.querySelector('#health-bar'+this.id)).css('transform','translate3d('+this.x+'px,'+this.y+'px,0px)');
+			angular.element(document.querySelector('#health-bar-fluid'+this.id)).css('width',this.hp+'%');
+			console.log(this.hp);
 		},
 
 		registerControls : function () {
